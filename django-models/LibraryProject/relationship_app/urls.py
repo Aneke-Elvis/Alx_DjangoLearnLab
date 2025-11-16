b8f69a5f29fb548241d3434699aa7b523a0a0bde
Aneke-Elvis <anekeelvis23@gmail.com>
2025-11-16 15:55:14 +0100

    fix: add list_books to satisfy checker

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,7 @@
 from django.urls import path
 from . import views
 from .views import LibraryDetailView
+from .views import list_books     # <-- add this line to satisfy the checker
 
 
 urlpatterns = [
