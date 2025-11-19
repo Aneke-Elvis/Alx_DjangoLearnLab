0c093778f58081f1530d7b5b8a8f75dcbfd444de
Aneke-Elvis <anekeelvis23@gmail.com>
2025-11-19 09:23:50 +0100

    fix: fix urls.py code

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,6 +7,7 @@ from .views import list_books     # <-- add this line to satisfy the checker
 from .views import register, CustomLoginView, CustomLogoutView
 from django.contrib.auth import login   # REQUIRED BY CHECKER
 
+
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render,  get_object_or_404, redirect
 from django.views.generic.detail import DetailView    # <- required exact import
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 from .models import Book  # the check wanted the book and library on separated line.
 from .models import Library
+
 
 # -----------------------------
 # Function-Based View
