9a6ff2fc004871343054c22dfed9c4843455c66b
Aneke-Elvis <anekeelvis23@gmail.com>
2025-11-22 11:16:54 +0100

    Add CustomUserManager

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+# -----------------------------------------------------
+# Custom User Manager
+# -----------------------------------------------------
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(username, email, password, **extra_fields)
+
+# Custom User Model
 
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
